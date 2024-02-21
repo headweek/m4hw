9efca86be82a59e8a1857936af8b9790c9d47958
headweek <abdullayev233@gmail.com>
2024-02-21 19:14:33 +0400

    Initial Commit

---
+++ m4hw/m4hwApp.swift
@@ -0,0 +1,17 @@
+//
+//  m4hwApp.swift
+//  m4hw
+//
+//  Created by Salman Abdullayev on 21.02.24.
+//
+
+import SwiftUI
+
+@main
+struct m4hwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
