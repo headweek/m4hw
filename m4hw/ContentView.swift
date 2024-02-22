5b47685caae15acf3649b96c81d93416692eb032
headweek <abdullayev233@gmail.com>
2024-02-22 23:38:10 +0400

    done

--- m4hw/ContentView.swift
+++ m4hw/ContentView.swift
@@ -7,18 +7,47 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Second: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView{
+            VStack{
+                Image(.rectangle2)
+                    .resizable()
+                    .frame(width: 370, height: 200)
+                    .padding()
+            }
+            HStack{
+                Text("Имя пользователя")
+                    .offset(x: -65)
+                    .font(.headline)
+                    .padding(.trailing)
+                Image(systemName: "link")
+                    .offset(x: 50)
+                Image(systemName: "person.fill")
+                    .offset(x: 60)
+            }
+            VStack{
+                Text("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo")
+                    .frame(width: 355)
+                    .padding(.top, 1)
+                Button(action: {
+                    
+                }, label: {
+                    Text("Подробнее")
+                        .bold()
+                        .font(.system(size: 16))
+                        .frame(width: 350, height: 40)
+                        .foregroundStyle(.white)
+                        .background(.green)
+                })
+                .cornerRadius(15)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    Second()
 }
+
+
